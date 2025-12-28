5b6e248caf1c7ca745973861777fec2535173f3a
mrzero1703 <phamhuyquang0@gmail.com>
2025-12-29 00:36:43 +0700

    fixPort

--- server.js
+++ server.js
@@ -142,6 +142,7 @@ app.get("/history", (req, res) => {
 });
 
 // Port
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
